84a88e13350a27a1bdf4f9c4535bdddb4161d6ee
SoklengRoeun <soklengroeun92@gmail.com>
2026-03-04 18:14:22 +0700

    Update sequelizeConfig.js

--- src/config/sequelizeConfig.js
+++ src/config/sequelizeConfig.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'backend_pos_db',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DATABASE_URL || 'mysql://root:PPyRylTqpWFRCeOXaLqgWZcpNreYlZtr@mysql.railway.internal:3306/railway',
   {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || 'mysql'
+    dialect: 'mysql'
   }
 );
+
+//Old localhost conection
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || 'backend_pos_db',
+//   process.env.DB_USER || 'root',
+//   process.env.DB_PASSWORD || '',
+//   {
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || 3306,
+//     dialect: process.env.DB_DIALECT || 'mysql'
+//   }
+// );
 
 module.exports = sequelize;
